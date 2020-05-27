c8720a60974ae47c54e583373468122b214f0b34
Sheraram Prajapat <sheraramprajapat1998@gmail.com>
2020-05-27 18:47:01 +0530

    contact model added

--- mypf/models.py
+++ mypf/models.py
@@ -39,3 +39,18 @@ class Images(models.Model):
     image = models.ImageField(upload_to='project')
     multi_image = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='other_images')
     
+    class Meta:
+        verbose_name_plural = 'Images'
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    sent_time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-sent_time',)
+    
+    def __str__(self):
+        return self.name
