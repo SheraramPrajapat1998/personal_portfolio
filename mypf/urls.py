661f6ea91c23f77150e7e9c8bf9c26797166ac18
Sheraram Prajapat <sheraramprajapat1998@gmail.com>
2020-05-27 19:07:30 +0530

    contact view and form created

--- mypf/urls.py
+++ mypf/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_view, detail_view
+from .views import home_view, detail_view, contact
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('<int:year>/<int:month>/<int:day>/<slug:project>/', detail_view, name='detail'),
+    path('contact/', contact, name='contact'),
 
 ]
--- mypf/views.py
+++ mypf/views.py
@@ -10,3 +10,14 @@ def detail_view(request, year, month, day, project):
     project = get_object_or_404(Project, slug=project, status='Published', publish__year=year, publish__month=month, publish__day=day)
     images = project.other_images.all() # returns queryset of other images
     return render(request, 'mypf/detail.html', {'project':project, 'images':images})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'mypf/contact.html', {'form':form})
+    
